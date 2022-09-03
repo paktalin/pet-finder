f770cd62311ac6341b815d8dd29a65f6dd90c0ae
lita_kornilova <lita.kornilova@fob-solutions.com>
2022-09-03 21:42:13 +0300

    refresh token

--- app/src/main/java/com/paktalin/petfinder/data/remote/PetFinderInterceptor.kt
+++ app/src/main/java/com/paktalin/petfinder/data/remote/PetFinderInterceptor.kt
@@ -1,36 +1,62 @@
 package com.paktalin.petfinder.data.remote
 
+import com.paktalin.petfinder.BuildConfig
 import com.paktalin.petfinder.data.local.DevicePreferences
-import com.paktalin.petfinder.usecase.RefreshAccessTokenUseCase
+import com.paktalin.petfinder.data.remote.dto.AccessTokenResponseDto
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import okhttp3.FormBody
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
-import okhttp3.internal.closeQuietly
-import timber.log.Timber
 import javax.inject.Inject
 
 class PetFinderInterceptor @Inject constructor(
-    private val refreshAccessTokenUseCase: RefreshAccessTokenUseCase,
     private val devicePreferences: DevicePreferences,
+    private val json: Json
 ) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request().newBuilder()
-            .apply {
-                runBlocking {
-                    addHeader("Authorization", "Bearer ${devicePreferences.getAccessToken()}")
-                }
-            }
-            .build()
-        Timber.e("request")
+        val token = runBlocking { devicePreferences.getAccessToken() }
+        val originalRequest = chain.request().newBuilder().apply {
+            token?.let(::addTokenHeader)
+        }.build()
 
-        val response = chain.proceed(request)
-        Timber.e("response: $response")
+        val response = chain.proceed(originalRequest)
         if (response.code == 401) {
-            runBlocking { refreshAccessTokenUseCase() }
-            response.closeQuietly()
+            val refreshResponse = refreshToken(originalRequest, chain)
+            if (refreshResponse.isSuccessful) {
+                val refreshedToken = decodeRefreshedToken(refreshResponse)
+                runBlocking { devicePreferences.setAccessToken(token) }
+                val newCall = originalRequest.newBuilder().addTokenHeader(refreshedToken).build()
+                return chain.proceed(newCall)
+            }
         }
-
         return response
     }
+
+    private fun refreshToken(request: Request, chain: Interceptor.Chain): Response {
+        val body = FormBody.Builder()
+            .add("grant_type", "client_credentials")
+            .add("client_id", "0NjzgAzMc14Gq1AkvypepRSA8q4V5LRAx8EwVFAeRIRiJNoyYS")
+            .add("client_secret", "x7XS0mz8YaA6zfOl03lr8QQJjNfhUAANlwODyTcT")
+            .build()
+
+        val refreshTokenRequest = request
+            .newBuilder()
+            .post(body)
+            .url("${BuildConfig.BASE_URL}oauth2/token")
+            .build()
+        return chain.proceed(refreshTokenRequest)
+    }
+
+    private fun decodeRefreshedToken(response: Response): String {
+        val body = response.body?.string() ?: return ""
+        return json.decodeFromString<AccessTokenResponseDto>(body).access_token
+    }
+}
+
+private fun Request.Builder.addTokenHeader(token: String): Request.Builder {
+    return addHeader("Authorization", "Bearer $token")
 }
